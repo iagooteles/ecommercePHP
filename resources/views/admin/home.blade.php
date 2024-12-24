551e368c4616c980327a58aa2d2a8dcee4975d11
Iago Teles <iagooteles@gmail.com>
2024-12-24 20:49:02 -0300

    added admin dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,3 +1,26 @@
-<x-app-layout>
-    
-</x-app-layout>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('admin/metadata')
+</head>
+
+<body>
+    <div class="container-scroller">
+
+        @include('admin/sidebar')
+
+        <div class="container-fluid page-body-wrapper">
+
+            @include('admin/header')
+
+            @include('admin/body')
+
+        </div>
+
+    </div>
+
+    @include('admin/scripts')
+</body>
+
+</html>
